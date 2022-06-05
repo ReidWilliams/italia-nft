73f68c5d04e7b3947ab2bfd4b03b60d1f9e19803
Reid Williams <reid@computable.io>
2022-06-06 01:53:38 +0200

    adding stickers works on opensea

--- nft.sol
+++ nft.sol
@@ -8,18 +8,27 @@ contract Italia is ERC721Enumerable, ReentrancyGuard, Ownable {
     string[] private _stickers;
 
     // maps a tokenId to an array of stickerIds
-    mapping(uint256 => uint256[]) private _tokenMapToStickers;
+    mapping(uint256 => uint256[]) private _tokenMapToStickerIds;
 
-    function tokenURI(uint256 tokenId) override public pure returns (string memory) {
-        string[1] memory parts;
-        parts[0] = '<svg width="124" height="36" viewBox="0 0 124 36" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M29.1765 0V7.2V14.4V21.6V28.8H21.8823V21.6V14.4V7.2H14.5882V14.4V21.6V28.8H7.29411V21.6V14.4V7.2V0H0V7.2V14.4V21.6V28.8V36H7.29411H14.5882H21.8823H29.1765H36.4706V28.8V21.6V14.4V7.2V0H29.1765Z" fill="black"/><path d="M80.2352 28.8H72.9411H65.647H58.3529H51.0588V21.6H58.3529H65.647H72.9411V14.4H65.647H58.3529H51.0588V7.2H58.3529H65.647H72.9411H80.2352V0H72.9411H65.647H58.3529H51.0588H43.7646V7.2V14.4V21.6V28.8V36H51.0588H58.3529H65.647H72.9411H80.2352V28.8Z" fill="black"/><path d="M87.5293 7.2H94.8234H102.118H109.412H116.706V14.4H109.412H102.118H94.8234V21.6H102.118H109.412H116.706V28.8H109.412H102.118H94.8234H87.5293V36H94.8234H102.118H109.412H116.706H124V28.8V21.6V14.4V7.2V0H116.706H109.412H102.118H94.8234H87.5293V7.2Z" fill="black"/></svg>';
+    function tokenURI(uint256 tokenId) override public view returns (string memory) {
+        string memory prefix = "<svg width='350' height='350' viewBox='0 0 350 350' fill='none' xmlns='http://www.w3.org/2000/svg'><rect width='97' height='100' fill='#D9D9D9'/><path d='M50.5 20L57.3477 41.075H79.5072L61.5798 54.1L68.4275 75.175L50.5 62.15L32.5725 75.175L39.4202 54.1L21.4928 41.075H43.6523L50.5 20Z' fill='#F21F1F'/>";
+        string memory suffix = '</svg>';
 
-        string memory output = string(abi.encodePacked(parts[0]));
+        string memory stickerSVGs = prefix;
 
-        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "WE3+DREAM+Avara Italia', toString(tokenId), '", "description": "this is a description", "image": "data:image/svg+xml;base64,', Base64.encode(bytes(output)), '"}'))));
-        output = string(abi.encodePacked('data:application/json;base64,', json));
+        uint256[] memory stickerIds = _tokenMapToStickerIds[tokenId];
+        for (uint i=0; i < stickerIds.length; i++) {
+            stickerSVGs = string.concat(stickerSVGs, _stickers[stickerIds[i]]);
+        }
+
+        stickerSVGs = string.concat(stickerSVGs, suffix);
+
+        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "Italia NFT', toString(tokenId), '", "description": "This is a description", "image": "data:image/svg+xml;base64,', Base64.encode(bytes(stickerSVGs)), '"}'))));
+        string memory output = string(abi.encodePacked('data:application/json;base64,', json));
 
         return output;
+
+
     }
 
     function claim(uint256 tokenId) public nonReentrant {
@@ -35,15 +44,13 @@ contract Italia is ERC721Enumerable, ReentrancyGuard, Ownable {
     }
 
     function addStickerToToken(uint256 stickerId, uint256 tokenId) public nonReentrant {
-        _tokenMapToStickers[tokenId].push(stickerId);
+        _tokenMapToStickerIds[tokenId].push(stickerId);
     } 
 
     function getSticker(uint256 stickerId) public view returns (string memory) {
         return _stickers[stickerId];
     }
 
-
-    
     function toString(uint256 value) internal pure returns (string memory) {
     // Inspired by OraclizeAPI's implementation - MIT license
     // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
