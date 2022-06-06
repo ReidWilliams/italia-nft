f02c762713d8e0f893b9b7cb95cbce23bdf86537
Reid Williams <reid@computable.io>
2022-06-06 12:31:28 +0200

    works on polygon and with sticker x,y

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # italia-nft
+
+OpenSea URL for NFTs on Polygon Mumbai testnet:
+
+https://testnets.opensea.io/assets/mumbai/{contract_address/{tokenId}
--- nft.sol
+++ nft.sol
@@ -3,12 +3,20 @@ pragma solidity ^0.8.0;
 
 import "./lib.sol";
 
+// TODO introduce soft delete to allow removal of sticker
+struct StickerInstance {
+    uint256 x;
+    uint256 y;
+    uint256 id;
+}
+
 contract Italia is ERC721Enumerable, ReentrancyGuard, Ownable {
 
-    string[] private _stickers;
+    // Stickers are just an SVG
+    string[] private _allStickers;
 
-    // maps a tokenId to an array of stickerIds
-    mapping(uint256 => uint256[]) private _tokenMapToStickerIds;
+    // maps a tokenId to an array of stickerInstances
+    mapping(uint256 => StickerInstance[]) public _tokenMapToStickerInstances;
 
     function tokenURI(uint256 tokenId) override public view returns (string memory) {
         string memory prefix = "<svg width='350' height='350' viewBox='0 0 350 350' fill='none' xmlns='http://www.w3.org/2000/svg'><rect width='97' height='100' fill='#D9D9D9'/><path d='M50.5 20L57.3477 41.075H79.5072L61.5798 54.1L68.4275 75.175L50.5 62.15L32.5725 75.175L39.4202 54.1L21.4928 41.075H43.6523L50.5 20Z' fill='#F21F1F'/>";
@@ -16,40 +24,42 @@ contract Italia is ERC721Enumerable, ReentrancyGuard, Ownable {
 
         string memory stickerSVGs = prefix;
 
-        uint256[] memory stickerIds = _tokenMapToStickerIds[tokenId];
-        for (uint i=0; i < stickerIds.length; i++) {
-            stickerSVGs = string.concat(stickerSVGs, _stickers[stickerIds[i]]);
+        StickerInstance[] memory stickerInstances = _tokenMapToStickerInstances[tokenId];
+
+        for (uint i=0; i < stickerInstances.length; i++) {
+            StickerInstance memory sticker = stickerInstances[i];
+
+            stickerSVGs = string.concat(stickerSVGs, '<g transform="translate(', toString(sticker.x), ', ', toString(sticker.y), ')">');
+            stickerSVGs = string.concat(stickerSVGs, _allStickers[sticker.id]);
+            stickerSVGs = string.concat(stickerSVGs, '</g>');
         }
 
         stickerSVGs = string.concat(stickerSVGs, suffix);
 
-        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "Italia NFT', toString(tokenId), '", "description": "This is a description", "image": "data:image/svg+xml;base64,', Base64.encode(bytes(stickerSVGs)), '"}'))));
+        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "Italia NFT", "description": "This is a description", "image": "data:image/svg+xml;base64,', Base64.encode(bytes(stickerSVGs)), '"}'))));
         string memory output = string(abi.encodePacked('data:application/json;base64,', json));
 
         return output;
-
-
     }
 
+    // TODO, just mint the next available
     function claim(uint256 tokenId) public nonReentrant {
         _safeMint(_msgSender(), tokenId);
     }
-    
-    function ownerClaim(uint256 tokenId) public nonReentrant onlyOwner {
-        _safeMint(owner(), tokenId);
-    }
 
     function addNewSticker(string calldata svgCode) public nonReentrant {
-        _stickers.push(svgCode);
+        _allStickers.push(svgCode);
     }
 
-    function addStickerToToken(uint256 stickerId, uint256 tokenId) public nonReentrant {
-        _tokenMapToStickerIds[tokenId].push(stickerId);
+    function addStickerToToken(
+        uint256 stickerId, 
+        uint256 x, 
+        uint256 y, 
+        uint256 tokenId) 
+    public nonReentrant {
+        // TODO, error if sticker id doesn't exist
+        _tokenMapToStickerInstances[tokenId].push(StickerInstance(x, y, stickerId));
     } 
-
-    function getSticker(uint256 stickerId) public view returns (string memory) {
-        return _stickers[stickerId];
-    }
 
     function toString(uint256 value) internal pure returns (string memory) {
     // Inspired by OraclizeAPI's implementation - MIT license
